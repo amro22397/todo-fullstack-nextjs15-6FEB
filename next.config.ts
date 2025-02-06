78c4d2c4721567ffd425684783297a6b91543bc5
amro22397 <amroalmutasim22@gmail.com>
2025-02-06 12:21:25 +0400

    next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+        {
+            protocol: "https",
+            hostname: "**"
+        }
+    ]
+},
+// experimental: {
+//     missingSuspenseWithCSRBailout: false,
+//   },    
+  typescript: {
+        ignoreBuildErrors: true,
+      },
+  eslint: {
+        ignoreDuringBuilds: true,
+      }
 };
 
 export default nextConfig;
